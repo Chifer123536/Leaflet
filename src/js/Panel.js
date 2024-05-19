0c3b98e6f7b1f52a1f3048f89bafd4ffc2cc06c1
Chifer123536 <dun24821@gmail.com>
2024-05-19 17:09:02 +0300

    Обновил Panel.js, в test.js теперь только симуляция

--- src/js/Panel.js
+++ src/js/Panel.js
@@ -1,5 +1,19 @@
+import updateGpsGlobalInt from './update_gps_global_int.js'
+import updateSystemStatus from './update_system_status.js'
+import updateTime from './update_time.js'
+import updateServoOutputRaw from './update_servo_output_raw.js'
+import updateAttitude from './update_attitude.js'
+
 class Panel {
 	constructor() {
+		this.dataBuffer = {
+			gps: null,
+			time: null,
+			systemStatus: null,
+			servoOutput: null,
+			attitude: null,
+		}
+		this.currentIndex = 0
 		this.updateFunctions = []
 	}
 
@@ -9,6 +23,68 @@ class Panel {
 
 	update() {
 		this.updateFunctions.forEach(func => func())
+	}
+
+	handleMessage(message) {
+		switch (message.type) {
+			case 'gps':
+				this.dataBuffer.gps = message.data
+				break
+			case 'time':
+				this.dataBuffer.time = message.data
+				break
+			case 'systemStatus':
+				this.dataBuffer.systemStatus = message.data
+				break
+			case 'servoOutput':
+				this.dataBuffer.servoOutput = message.data
+				break
+			case 'attitude':
+				this.dataBuffer.attitude = message.data
+				break
+			default:
+				console.error('Unknown message type:', message.type)
+		}
+		this.update()
+	}
+
+	updateData() {
+		if (this.dataBuffer.gps) {
+			updateGpsGlobalInt(this.dataBuffer.gps)
+		}
+		if (this.dataBuffer.time) {
+			updateTime(this.dataBuffer.time)
+		}
+		if (this.dataBuffer.systemStatus) {
+			updateSystemStatus(this.dataBuffer.systemStatus)
+		}
+		if (this.dataBuffer.servoOutput) {
+			updateServoOutputRaw(this.dataBuffer.servoOutput)
+		}
+		if (this.dataBuffer.attitude) {
+			updateAttitude(this.dataBuffer.attitude)
+		}
+		this.checkGpsData()
+	}
+
+	checkGpsData() {
+		const messageElement = document.querySelector('.centered-message')
+		if (
+			!this.dataBuffer.gps ||
+			this.dataBuffer.gps.lat == null ||
+			this.dataBuffer.gps.lon == null
+		) {
+			if (!messageElement) {
+				const message = document.createElement('div')
+				message.innerHTML = 'no GPS'
+				message.classList.add('centered-message')
+				document.getElementById('map').appendChild(message)
+			}
+		} else {
+			if (messageElement) {
+				messageElement.remove()
+			}
+		}
 	}
 }
 
--- src/js/test.js
+++ src/js/test.js
@@ -1,157 +1,73 @@
 import Panel from './Panel.js'
 import { routeCoordinates } from './route.js'
 import * as Random from './getRandom.js'
-import update_gps_global_int from './update_gps_global_int.js'
-import update_system_status from './update_system_status.js'
-import update_time from './update_time.js'
-import update_servo_output_raw from './update_servo_output_raw.js'
-import update_attitude from './update_attitude.js'
 
-class DataPanel {
-	constructor() {
-		this.panel = new Panel()
-		this.dataBuffer = {
-			gps: null,
-			time: null,
-			systemStatus: null,
-			servoOutput: null,
-			attitude: null,
-		}
-		this.currentIndex = 0
+const panel = new Panel()
 
-		this.panel.addUpdateFunction(this.updateData.bind(this))
+panel.addUpdateFunction(panel.updateData.bind(panel))
+
+function createGpsMessage() {
+	if (routeCoordinates.length === 0) {
+		return { type: 'gps', data: null }
+	}
+	const currentCoordinates = routeCoordinates[panel.currentIndex]
+	const nextCoordinates =
+		routeCoordinates[(panel.currentIndex + 1) % routeCoordinates.length]
+	const azimuth = Random.getAzimuth(
+		currentCoordinates[0],
+		currentCoordinates[1],
+		nextCoordinates[0],
+		nextCoordinates[1]
+	)
+
+	const gpsData = {
+		lat: currentCoordinates[0],
+		lon: currentCoordinates[1],
+		alt: Random.getRandom_gps_global_int().alt,
+		vel: Random.getRandom_gps_global_int().vel,
+		satellites_visible: Random.getRandom_gps_global_int().satellites_visible,
+		cog: azimuth,
 	}
 
-	updateData() {
-		if (this.dataBuffer.gps) {
-			update_gps_global_int(this.dataBuffer.gps)
-		}
-		if (this.dataBuffer.time) {
-			update_time(this.dataBuffer.time)
-		}
-		if (this.dataBuffer.systemStatus) {
-			update_system_status(this.dataBuffer.systemStatus)
-		}
-		if (this.dataBuffer.servoOutput) {
-			update_servo_output_raw(this.dataBuffer.servoOutput)
-		}
-		if (this.dataBuffer.attitude) {
-			update_attitude(this.dataBuffer.attitude)
-		}
-		this.checkGpsData()
-	}
-
-	checkGpsData() {
-		const messageElement = document.querySelector('.centered-message')
-		if (
-			!this.dataBuffer.gps ||
-			this.dataBuffer.gps.lat == null ||
-			this.dataBuffer.gps.lon == null
-		) {
-			if (!messageElement) {
-				const message = document.createElement('div')
-				message.innerHTML = 'no GPS'
-				message.classList.add('centered-message')
-				document.getElementById('map').appendChild(message)
-			}
-		} else {
-			if (messageElement) {
-				messageElement.remove()
-			}
-		}
-	}
-
-	handleMessage(message) {
-		switch (message.type) {
-			case 'gps':
-				this.dataBuffer.gps = message.data
-				break
-			case 'time':
-				this.dataBuffer.time = message.data
-				break
-			case 'systemStatus':
-				this.dataBuffer.systemStatus = message.data
-				break
-			case 'servoOutput':
-				this.dataBuffer.servoOutput = message.data
-				break
-			case 'attitude':
-				this.dataBuffer.attitude = message.data
-				break
-			default:
-				console.error('Unknown message type:', message.type)
-		}
-		this.panel.update()
-	}
-
-	createGpsMessage() {
-		if (routeCoordinates.length === 0) {
-			return { type: 'gps', data: null }
-		}
-		const currentCoordinates = routeCoordinates[this.currentIndex]
-		const nextCoordinates =
-			routeCoordinates[(this.currentIndex + 1) % routeCoordinates.length]
-		const azimuth = Random.getAzimuth(
-			currentCoordinates[0],
-			currentCoordinates[1],
-			nextCoordinates[0],
-			nextCoordinates[1]
-		)
-
-		const gpsData = {
-			lat: currentCoordinates[0],
-			lon: currentCoordinates[1],
-			alt: Random.getRandom_gps_global_int().alt,
-			vel: Random.getRandom_gps_global_int().vel,
-			satellites_visible: Random.getRandom_gps_global_int().satellites_visible,
-			cog: azimuth,
-		}
-
-		this.currentIndex = (this.currentIndex + 1) % routeCoordinates.length
-		return { type: 'gps', data: gpsData }
-	}
-
-	createTimeMessage() {
-		return { type: 'time', data: Random.getRandom_update_time() }
-	}
-
-	createSystemStatusMessage() {
-		return { type: 'systemStatus', data: Random.generateRandomSystemStatus() }
-	}
-
-	createServoOutputMessage() {
-		return { type: 'servoOutput', data: Random.getRandom_servo_output_raw() }
-	}
-
-	createAttitudeMessage() {
-		return { type: 'attitude', data: Random.getRandom_attitude() }
-	}
-
-	simulateIncomingMessage() {
-		const messageTypes = [
-			this.createGpsMessage.bind(this),
-			this.createTimeMessage.bind(this),
-			this.createSystemStatusMessage.bind(this),
-			this.createServoOutputMessage.bind(this),
-			this.createAttitudeMessage.bind(this),
-		]
-		const randomIndex = Math.floor(Math.random() * messageTypes.length)
-		const message = messageTypes[randomIndex]()
-		this.handleMessage(message)
-	}
+	panel.currentIndex = (panel.currentIndex + 1) % routeCoordinates.length
+	return { type: 'gps', data: gpsData }
 }
 
-const dataPanel = new DataPanel()
+function createTimeMessage() {
+	return { type: 'time', data: Random.getRandom_update_time() }
+}
 
-// Симуляция получения сообщений с различными интервалами
-setInterval(() => dataPanel.simulateIncomingMessage(), 500) // каждые 0.5 секунды
+function createSystemStatusMessage() {
+	return { type: 'systemStatus', data: Random.generateRandomSystemStatus() }
+}
 
-// Начальная проверка данных GPS
-dataPanel.checkGpsData()
+function createServoOutputMessage() {
+	return { type: 'servoOutput', data: Random.getRandom_servo_output_raw() }
+}
+
+function createAttitudeMessage() {
+	return { type: 'attitude', data: Random.getRandom_attitude() }
+}
+
+function simulateIncomingMessage() {
+	const messageTypes = [
+		createGpsMessage,
+		createTimeMessage,
+		createSystemStatusMessage,
+		createServoOutputMessage,
+		createAttitudeMessage,
+	]
+	const randomIndex = Math.floor(Math.random() * messageTypes.length)
+	const message = messageTypes[randomIndex]()
+	panel.handleMessage(message)
+}
+
+setInterval(() => simulateIncomingMessage(), 500)
+
+panel.checkGpsData()
 
 try {
-	// Начальная симуляция данных и обновление панели
-	dataPanel.panel.update()
+	panel.update()
 } catch (error) {
 	console.error('An error occurred:', error)
 }
