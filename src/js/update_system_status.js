89e96463c8891f771b41084f3d780234d31bb61f
Chifer123536 <116970649+Chifer123536@users.noreply.github.com>
2024-06-03 20:38:05 +0300

    Update update_system_status.js

--- src/js/update_system_status.js
+++ src/js/update_system_status.js
@@ -1,11 +1,9 @@
-// Экспорт функции update_system_status
 function update_system_status(data) {
 	const batteryChargeElement = document.getElementById('batteryCharge')
 	const batteryVoltageElement = document.getElementById('batteryVoltage')
 	const batteryCurrentElement = document.getElementById('batteryCurrent')
 	const flightModeElement = document.getElementById('flightMode')
 
-	// Обновление DOM элементов с использованием переданных данных
 	batteryChargeElement.textContent = data.batteryCharge
 	batteryVoltageElement.textContent = data.batteryVoltage
 	batteryCurrentElement.textContent = data.batteryCurrent
